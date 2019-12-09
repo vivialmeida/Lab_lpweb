8b217ee8aca3885ce4d8187d712f0d23a25e30bd
vivialmeida <viviane_k5@hotmail.com>
2019-12-08 23:06:54 -0300

    Retornando o preco X quantidade

--- tarefa04/script/pedido.js
+++ tarefa04/script/pedido.js
@@ -1,44 +1,42 @@
 let quantidade = document.querySelector('#qtd');
 let codigo = document.querySelector('#cod');
 let campo_total= document.querySelector('#total');
-let td_produtos = document.querySelectorAll('')
+let td_produtos = document.querySelectorAll('td')
 
 
 
 document.querySelector('#calcular').addEventListener(
     'click', function(event){
             event.preventDefault();
-            setTotal();
+            setTotal()
     });
 
 
 function calcula(){
- return (quantidade.valueAsNumber * codigo.valueAsNumber);
+    
+ return parseFloat(encontrarValorPorCodigo()) * parseFloat(quantidade.valueAsNumber);
+
+    
 }
   
 
 function setTotal(){
     campo_total.innerHTML ="R$ " + calcula();
-    encontrarCodigo();
+    
+
 
 }
 
 
-function encontrarCodigo(){
-
-    // console.log (td_produtos.length)
-   td_produtos.forEach(element => {
-       if(element.textContent = codigo.value){
-           console.
-       }
-       
-   });
- };
-
-        
-
-
-
-
-
-
+function encontrarValorPorCodigo(){
+    let valor;
+    for(let i= 0; i<td_produtos.length; i++){
+            if(td_produtos[i].textContent== codigo.value){
+             valor = (td_produtos[i +1].textContent).replace(",", ".");
+               parseFloat(valor);
+               break;
+            }
+   }
+   console.log(typeof(valor))
+   return valor;
+}
