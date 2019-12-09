0bd8e55de4ee57b351b379d87e347179145488f7
vivialmeida <viviane_k5@hotmail.com>
2019-12-08 23:26:40 -0300

    Adicionando tarefa 5

---
+++ tarefa5/script/script.js
@@ -0,0 +1,20 @@
+function transformaDataEmExtenso(event) {
+	event.preventDefault();
+	let data = document.getElementById("data").value;
+	let corpo = document.getElementById("js-divResultado");
+
+	corpo.innerHTML  = "<p style='font-size: 20px'>" + transformaData(data) +"</p>";
+}
+
+function transformaData(data) {
+	let meses = [
+		'Janeiro', 'Fevereiro', 'Março', 
+		'Abril', 'Maio', 'Junho',
+		'Julho', 'Agosto', 'Setembro',
+		'Outubro', 'Novembro', 'Dezembro'
+	];
+
+	let dt = data.split('/');
+
+	return dt[0] + ' de ' + meses[parseInt(dt[1]) - 1] + ' de ' + dt[2];
+}
